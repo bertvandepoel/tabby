75ab9c8d0f69938240146989b6832d4c20a5ee65
Bert Van de Poel <bert@bhack.net>
2019-11-19 02:01:10 +0100

    Fix layout issues with today button in people card

--- resources/uiblocks.php
+++ resources/uiblocks.php
@@ -159,7 +159,7 @@ function peoplecard($card) {
 							<input type="date" class="form-control" id="<?php echo $card['email']; ?>-date" name="date">
 						</div>
 						<div class="col-md-1 px-1">
-							<button class="btn btn-secondary" type="button" onclick="document.getElementById('<?php echo $card['email']; ?>-date').value = '<?php echo date('Y-m-d'); ?>'"><span class="fas fa-calendar-check"></span></button>
+							<button class="btn btn-secondary btn-sm mt-1" type="button" onclick="document.getElementById('<?php echo $card['email']; ?>-date').value = '<?php echo date('Y-m-d'); ?>'"><span class="fas fa-calendar-check"></span></button>
 						</div>
 						<div class="col-md-4 px-1">
 							<input type="text" class="form-control" name="comment" placeholder="Wire transfer">
